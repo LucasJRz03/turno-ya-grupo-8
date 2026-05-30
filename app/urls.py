547d352ba5eb0df38b00dc3893442699ea768964
LucasJRz03 <rodriguezlucasjesus196@gmail.com>
2026-05-30 18:32:05 -0300

    feat: add urls faltantes

--- app/urls.py
+++ app/urls.py
@@ -6,9 +6,16 @@ from . import views
 app_name = "app"
 
 urlpatterns = [
+    # Inicio
     path("", views.HomeView.as_view(), name="home"),
+    # Medicos
     path("medicos/", views.ListaMedicosView.as_view(), name="lista_medicos"),
+    path("medicos/<int:pk>/", views.DetalleMedicoView.as_view(), name="detalle_medico"),
+    # Turnos
+    path("turnos/", views.TurnoListView.as_view(), name="lista_turnos"),
     path("turnos/nuevo/", views.TurnoCreateView.as_view(), name="nuevo_turno"),
+    path("turnos/<int:pk>/cancelar/", views.CancelarTurnoView.as_view(), name="cancelar_turno")   
+
     # TODO:
     # path("medicos/<int:pk>/", views.DetalleMedicoView.as_view(), name="detalle_medico"),
     # path("turnos/", views.ListaTurnosView.as_view(), name="lista_turnos"),
