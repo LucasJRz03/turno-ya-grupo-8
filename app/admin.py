e203e9b9898f7b62f4aa0875963b7c249785c2dd
LucasJRz03 <rodriguezlucasjesus196@gmail.com>
2026-06-18 13:22:34 -0300

    refactor: admin

--- app/admin.py
+++ app/admin.py
@@ -1,27 +1,29 @@
 """Configuración básica del admin para los modelos de la app."""
 
 from django.contrib import admin
+from unfold.admin import ModelAdmin 
 from .models import Medico, Especialidad, Paciente, Turno
 
+
 @admin.register(Especialidad)
-class EspecialidadAdmin(admin.ModelAdmin):
+class EspecialidadAdmin(ModelAdmin):
     list_display = ( "nombre", "descripcion")
     search_fields = ("nombre",)
 
 @admin.register(Medico)
-class MedicoAdmin(admin.ModelAdmin):
+class MedicoAdmin(ModelAdmin):
     list_display = ("apellido", "nombre", "matricula", "especialidad")
     search_fields = ("apellido", "nombre", "matricula")
     list_filter = ("especialidad",)
     ordering = ("apellido", "nombre")
 
 @admin.register(Paciente)
-class PacienteAdmin(admin.ModelAdmin):
+class PacienteAdmin(ModelAdmin):
     list_display = ("apellido", "nombre", "dni", "email", "telefono")
     search_fields = ("apellido", "nombre", "dni")
 
 @admin.register(Turno)
-class TurnoAdmin(admin.ModelAdmin):
+class TurnoAdmin(ModelAdmin):
     list_display = ("fecha_hora", "medico", "paciente","estado")
     list_filter = ("estado", "fecha_hora", "medico")
     search_fields = ("paciente__apellido", "paciente__dni","medico__apellido", "motivo")
